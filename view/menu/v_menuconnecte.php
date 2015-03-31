5f314e0880088d1980468367470ed8edad6d1c41
sacrece <pedrosacrece@gmail.com>
2015-03-31 15:30:48 +0200

    modif menu connecté
    rajout de mon panier et historique de vente

--- view/menu/v_menuconnecte.php
+++ view/menu/v_menuconnecte.php
@@ -3,10 +3,10 @@
         			<ul>
         				<li><a href="http://localhost/NeighborFood/controler/accueil/c_accueil.php">Home</a></li>
         				<li><a href="#">Forum</a> </li>
-        				
-        				
+        				<li><a href="#">Mon panier</a> </li>
         				<li><a href="#">Acheter</a> </li>
 						<li><a href="http://localhost/NeighborFood/controler/vente/c_vente.php">Vendre</a></li>
+						<li><a href="#">historique de vente</a> </li>
         			</ul>
         		</nav>
         	</div>
