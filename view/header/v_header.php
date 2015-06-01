1c48eca593b849080e41be7113c2b3aa88768c15
Armel <armeldlm@hotmail.fr>
2015-06-01 12:03:07 +0200

    js
    javascript pour l'accueil : caroussel

--- view/header/v_header.php
+++ view/header/v_header.php
@@ -1,13 +1,36 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <link rel="stylesheet" href="/NeighborFood/view/css/accueil/accueil.css" /> <!-- associe cette page au page css stylessheet-->
-        <title>NeihgborFood - ???????</title>
-    </head>
-    
-    
-    	<body>
-    		<header>
-        		
-        	</header>
+<link href = "/NeighborFood/js/carousel/owl.carousel.css" rel="stylesheet">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="/NeighborFood/view/css/accueil/accueil.css" /> <!-- associe cette page au page css stylessheet-->
+    <title>NeihgborFood - ???????</title>
+</head>
+
+
+<body>
+<header>
+    <div class="carousel">
+        <div class="item">
+            <img src="/NeighborFood/view/image/fruitsban1.jpg" width="100%" >
+        </div>
+        <div class="item">
+            <img src="/NeighborFood/view/image/fruitsban2.jpg"  width="100%">
+        </div>
+        <div class="item">
+            <img src="/NeighborFood/view/image/fruitsban3.jpg"  width="100%">
+        </div>
+        <div class="item">
+            <img src="/NeighborFood/view/image/fruitsban4.jpg"  width="100%">
+        </div>
+
+    </div>
+</header>
+
+<script src="/NeighborFood/js/jquery-1.11.3.min.js"></script>
+<script src = "/NeighborFood/js/carousel/owl.carousel.min.js "></script>
+<script src = "/NeighborFood/js/j_carousel.js"></script>
+
+
+
+
