2777f708d19761e239324f32bb3093687569da3d
Armel <armeldlm@hotmail.fr>
2015-06-01 11:49:42 +0200

    css vente
    css sur la vente

--- modele/admin/m_admin.php
+++ modele/admin/m_admin.php
@@ -3,13 +3,11 @@
 require_once "../../modele/connexionBdd/m_connexionBdd.php";
 function getuser(){
     $bdd = connexion_bdd();
-    $reponse = $bdd ->query( 'SELECT idmembre,name,first_name,email FROM member WHERE idmembre != "13" ');
+    $reponse = $bdd ->query( 'SELECT idmembre,name,first_name,email FROM member WHERE email != "admin@gmail.com" ');
     return $reponse;
 
 
 }
-
-
 
 
 ?>
--- view/vente/v_vente.php
+++ view/vente/v_vente.php
@@ -1,13 +1,17 @@
+<head>
 
+    <link rel="stylesheet" href="/NeighborFood/view/css/vente/vente.css" /> <!-- associe cette page au page css stylessheet-->
+
+</head>
 	<h2>VENDRE</h2>
 	<form name="vente" method="post" action="/NeighborFood/controler/vente/c_vente.php">
 	<table  id="vente" >
 	
 		<tr><td><label for="quantity">Quantité</label> : <input type="number" name="quantite" id="quantity"/></tr></td>
-		<tr><td><label for="weight">Poids</label> : <input type="number" name='poids' id="weight"/>Kg</tr></td>
+		<tr><td><label for="weight">Poids</label> : <input type="number" name='poids' id="weight"/> Kg</tr></td>
 		<tr><td><label for="description">Description du produit</label><br />
-			<textarea name="description" id="description"></textarea></tr></td>
-		<tr><td><label for="price">Prix</label> : <input type="number" name="prix" id="prix"/>Euros/Kg</tr></td>
+			<textarea name="description" id="description" cols="45" rows="5"></textarea></tr></td>
+		<tr><td><label for="price">Prix</label> : <input type="number" name="prix" id="prix"/> Euros/Kg</tr></td>
 		<tr><td>Voulez-vous vendre ou échanger votre produit ?<br/>
 			<input type="radio" name="buyorchange" value="buy" id="buy" /> <label for="buy">A vendre</label><br />
 			<input type="radio" name="buyorchange" value="change" id="change" /> <label for="change">A échanger</label></tr></td>
