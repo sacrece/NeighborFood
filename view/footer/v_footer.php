0d7b00cda7bdfca2e9369c84408517fd47745dac
Armel <armeldlm@hotmail.fr>
2015-06-09 15:24:05 +0200

    lien contacter nous

--- view/footer/v_footer.php
+++ view/footer/v_footer.php
@@ -4,7 +4,7 @@
         			<ul>
         				<help>
         				<li><a href="../../controler/aideEnLigne/c_accueilAide.php">Aide en ligne</a></li>
-        				<li><a href="#">Contacter nous</a></li>
+        				<li><a href="/NeighborFood/controler/contact/c_ecrire.php?emaildesti=admin@gmail.com">Contacter nous</a></li>
         				</help>
         			</ul>
         		</div>
